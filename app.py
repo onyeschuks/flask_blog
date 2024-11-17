d0e7226a1937681b3958609ff43fb49a2b1947cd
FearGod12 <onyenikechukwudi@.gmail.com>
2024-11-17 18:06:42 +0100

    added gunicorn

--- app.py
+++ app.py
@@ -128,5 +128,5 @@ def add_comment(post_id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=port)
